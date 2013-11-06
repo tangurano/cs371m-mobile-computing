7504a76fa5b8b903ff0f77453ada8aea2460292f
Kevin Jia <nunnjia@gmail.com>
2013-11-05 21:13:09 -0600

    media controller stuff

---
+++ NoteSync/src/com/cs371m/notesync/PlaybackController.java
@@ -0,0 +1,88 @@
+package com.cs371m.notesync;
+
+import android.widget.MediaController.MediaPlayerControl;
+
+public class PlaybackController implements MediaPlayerControl{
+
+	PlaybackService service;
+	String mediaPath;
+	
+	public PlaybackController(PlaybackService ps) {
+		service = ps;
+	}
+	
+	public void setMediaPath(String path) {
+		mediaPath = path;
+	}
+	
+	@Override
+	public boolean canPause() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean canSeekBackward() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean canSeekForward() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public int getAudioSessionId() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int getBufferPercentage() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int getCurrentPosition() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int getDuration() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public boolean isPlaying() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void pause() {
+		if (service != null)
+			service.Pause();
+	}
+
+	@Override
+	public void seekTo(int arg0) {
+		if (service != null)
+			service.Seek(arg0);
+	}
+
+	@Override
+	public void start() {
+		if (service != null)
+			service.Play(mediaPath);
+	}
+	
+	public void stop() {
+		if (service != null)
+			service.Stop();
+	}
+}
