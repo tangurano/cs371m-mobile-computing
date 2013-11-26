c0fed33c65ff5909aae32b1b8b6e6d6042b8b82c
Jiarui Hou <jzh101@cs.utexas.edu>
2013-11-25 23:00:29 -0600

    Working seekTo()

--- NoteSync/src/com/cs371m/notesync/MyImageView.java
+++ NoteSync/src/com/cs371m/notesync/MyImageView.java
@@ -2,7 +2,11 @@ package com.cs371m.notesync;
 
 import java.util.ArrayList;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.graphics.Canvas;
 import android.graphics.Paint;
 import android.os.Bundle;
@@ -16,6 +20,7 @@ import android.view.GestureDetector.SimpleOnGestureListener;
 import android.view.MotionEvent;
 import android.widget.ImageView;
 import android.widget.MediaController;
+import android.widget.Toast;
 
 public class MyImageView extends ImageView {
 
@@ -25,7 +30,7 @@ public class MyImageView extends ImageView {
 	MediaController myMediaController;
 	PlaybackController myPController;
 	private static final String DEBUG_TAG = "MyImageView";
-
+	private static final float THRESHOLD=5;
 	public MyImageView(Context context, AttributeSet attrs)
 	{
 		super(context, attrs);
@@ -70,42 +75,118 @@ public class MyImageView extends ImageView {
 		final Context myContext;
 		public GestureListener(Context context)
 		{
-		    myContext = context;
+			myContext = context;
 		}
-			@Override
-			public boolean onDown(MotionEvent e)
-			{
-				Log.v(DEBUG_TAG, "Down tapped");
-				return true;
-			}
-			@Override
-			public boolean onSingleTapUp(MotionEvent e)
-			{
-				super.onSingleTapUp(e);
-				Log.v(DEBUG_TAG, "Single Tap up");
-				myMediaController.show();
-				return true;
-			}
+		@Override
+		public boolean onDown(MotionEvent e)
+		{
+			Log.v(DEBUG_TAG, "Down tapped");
+			return true;
+		}
+		@Override
+		public boolean onSingleTapUp(MotionEvent e)
+		{
+			super.onSingleTapUp(e);
+			Log.v(DEBUG_TAG, "Single Tap up");
 
-			@Override
-			public void onLongPress(MotionEvent event)
-			{
-				//If we're here onDown() has been called
+			//If around visual mark
+			float eventX=e.getX();
+			float eventY=e.getY();
+			Point tP1=new Point(eventX, eventY);
+			//Call distance formula
 
-				Log.v(DEBUG_TAG, "Longpressed");
-				if (activity != null && activity.mCurrentNote != null)
+			if (activity != null && activity.mCurrentNote != null 
+					&& myPController!=null && activity.mCurrentNote.bookmarks != null)
+				
+			{
+
+				for (int i=0; i< activity.mCurrentNote.bookmarks.size();i++)
 				{
-					if (activity.mCurrentNote.bookmarks == null)
-						activity.mCurrentNote.bookmarks = new ArrayList<Point>();
-					if (activity.mCurrentNote.bookmarks.size() < activity.mCurrentNote.timestamps.size()) {
-						Point point = new Point(event.getX(), event.getY());
-						activity.mCurrentNote.bookmarks.add(point);
-						invalidate();
-						Log.d(DEBUG_TAG, "point: " + point);
-					}
-
+					if (Point.getDistance(tP1, activity.mCurrentNote.bookmarks.get(i)) < 20+THRESHOLD)
+					{	
+						long eachTime= activity.mCurrentNote.timestamps.get(i);
+						myPController.seekTo((int) eachTime);
+						myMediaController.show();
+						
+					}		
 				}
+			}			
+
+			//Seek to that point
+			//Call 
+
+			myMediaController.show();
+			return true;
+		}
+
+
+
+		@Override
+		public void onLongPress(MotionEvent event)
+		{
+			//If we're here onDown() has been called
+
+			Log.v(DEBUG_TAG, "Longpressed");
+			if (activity != null && activity.mCurrentNote != null)
+			{
+				if (activity.mCurrentNote.bookmarks == null)
+					activity.mCurrentNote.bookmarks = new ArrayList<Point>();
+				if (activity.mCurrentNote.bookmarks.size() < activity.mCurrentNote.timestamps.size()) 
+				{	
+					//Are you sure to make this mark?
+					//Create confirmation dialog
+					confirmDialog dialog=new confirmDialog();
+		
+					Point point = new Point(event.getX(), event.getY());
+					activity.mCurrentNote.bookmarks.add(point);
+					invalidate();
+					Log.d(DEBUG_TAG, "marked point: " + point);
+				}
+
 			}
+		}
+		/*
+		public void drawCircle(Point p)
+		{
+			
+		}
+		*/
+		public class confirmDialog extends DialogFragment 
+		{
+			
+			//Source:http://developer.android.com/guide/topics/ui/dialogs.html
+			@Override
+		    public Dialog onCreateDialog(Bundle savedInstanceState) 
+			{
+		        // Use the Builder class for convenient dialog construction
+		        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+		        builder.setMessage(R.string.confirmAddNote)
+		               .setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
+		                   public void onClick(DialogInterface dialog, int id) 
+		                   {
+		                	  
+		                	 /*  
+		                	 drawCircle();
+		   					activity.mCurrentNote.bookmarks.add(point);
+		   					invalidate();
+		   					Log.d(DEBUG_TAG, "marked point: " + point);	
+		   					*/
+		                	   Log.v(DEBUG_TAG, "OK!!");
+		                	   
+		                	  
+		                   }
+		               })
+		               .setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
+		                   public void onClick(DialogInterface dialog, int id) {
+		                       // User cancelled the dialog
+		                	   Log.v(DEBUG_TAG, "CANCEL!!");
+		                   }
+		               });
+		        // Create the AlertDialog object and return it
+		        return builder.create();
+		    }
+
+		}
 
 	}
 
--- NoteSync/src/com/cs371m/notesync/Point.java
+++ NoteSync/src/com/cs371m/notesync/Point.java
@@ -8,4 +8,9 @@ public class Point {
 		this.x = x;
 		this.y = y;
 	}
+	
+	public static float getDistance(Point p1, Point p2)
+	{
+		return (float) Math.sqrt(Math.pow(p1.x-p2.x, 2)+Math.pow(p1.y-p2.y, 2));
+	}
 }
